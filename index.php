0ecc40ceb325a40c2eaf4b04bd1f406d22224e0e
domingosjadriano <domingosadriano123@gmail.com>
2023-08-31 09:03:17 -0300

    index feito com sucesso

--- index.php
+++ index.php
@@ -1,11 +1,229 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title></title>
+	<title>Página Inicial da Multitocket</title>
+		<meta charset="utf-8">
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<link rel="stylesheet" type="text/css" href="css/all.min.css">
+		<meta name="description" content="sitecriadopordomingosadriano-domingosadriano123@gmail.com">
+		<meta name="keywords" content="ilidio,pureza,fotografo">
+		<meta name="author" content="Domingos JamAdri">
+		<meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0" />
 </head>
 <body>
+
+	<header>
+			<nav>
+				<div class="container">
+					<div class="flex1">
+						<div class="img-logo">
+							<div class="logo"></div><!--logo-->
+						</div>
+							<div class="menu">
+								<ul>
+									<li class="bottom"><a href="index.php">INICIO</a></li>
+									<li><a href="#">SOBRE</a></li>
+									<li><a href="#">SERVIÇOS</a></li>
+									<li><a href="portfolio.php">PORTFOLIO</a></li>
+									<li><a href="#">CONTACTO</a></li>
+								</ul>
+								<div class="rede">
+									<a href="#" style="color: orange;font-size: 20px;"><i class="fab fa-facebook"></i></a>
+									<a href="#" style="color: orange;font-size: 20px;"><i class="fab fa-whatsapp"></i></a>
+								</div><!--rede-->
+							</div><!--menu-->
+
+							<div class="menu-mobile">
+								<i class="fa fa-bars"></i>
+								<ul>
+									<li><a href="index.php">INICIO</a></li>
+									<li><a href="#">SOBRE</a></li>
+									<li><a href="#">SERVIÇOS</a></li>
+									<li><a href="portfolio.php">PORTFOLIO</a></li>
+									<li><a href="#">CONTACTO</a></li>
+								</ul>
+
+								<div class="rede">
+									<a href="#" style="color: orange;font-size: 20px;"><i class="fab fa-facebook"></i></a>
+									<a href="#" style="color: orange;font-size: 20px;"><i class="fab fa-whatsapp"></i></a>
+								</div><!--rede-->
+							</div><!--menu-mobile-->
+						</div><!--flex1-->
+
+					<div class="banner">
+						<div class="flex1">
+							<div class="texto">
+								<h2>Pulseiras</h2>
+								<h3>Pesonalizadas</h3>
+								<p>Seu evento ganha uma cara profisional e seus <br>clientes tem ótimas lembranças.</p>
+							</div><!--texto-->
+							<div class="img"></div><!--img-->
+						</div><!--flex1-->
+					</div><!--banner-->
+
+				</div><!--container-->
+			</nav>
+		</header>
+
+		<section class="one">
+			<div class="container">
+				<div class="flex1">
+					<div class="texto">
+						<h2>Solicite seu orçamento agora mesmo</h2>
+						<p>Nossa equipe de atendimento está a sua disposição</p>
+					</div><!--texto-->
+					<div class="botao">
+						<div class="btnTexto">
+							<a href="#">ORÇAMENTO</a>
+						</div><!--btnTexto-->
+					</div>
+				</div><!--flex1-->
+				<div class="clean"></div>
+			</div><!--container-->
+		</section><!--one-->
+
+		<section class="two">
+			<div class="container">
+				<div class="flex1">
+					<div class="bilhete">
+					
+					</div><!--bilhete-->
+					<div class="texto">
+						<h1>SOBRE A MULTI TICKET</h1>
+						<h2>Por que somos</h2>
+						<h3>a solução?</h3>
+						<p>Lorem ipsum dolor sit amet, consectetuer elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam esse molestie consequatadipiscing erat volelit esse molestie consequatadipiscing.</p>
+						<div class="btn">CONTINUE LENDO</div>
+					</div><!--texto-->
+				</div><!--flex1-->
+			</div><!--container-->
+		</section><!--two-->
+		<div class="clean"></div>
+
+		<section class="tree">
+			<div class="cover">
+				<div class="container">
+					<div class="flex1">
+						<div class="seta1 seta">QQ</div>
+						<div class="servicos">
+						<h1>QUAIS SÃO AS SUAS NECESSIDADES?</h1>
+						<h2>Preciso de ingressos personalizados <br/> para um festival de música</h2>
+						<div class="btn"><a href="#">VEJA NOSSOS SERVIÇOS</a></div>
+					</div><!--servicos-->
+					<div class="seta2 seta">QQ</div>
+					</div><!--flex1-->
+				</div><!--container-->
+			</div><!--cover-->
+		</section><!--tree-->
+
+		<section class="four">
+			<div class="container">
+				<div class="texto">
+					<h2>Conheça nossos serviços</h2>
+					<p>São várias soluções sob medida para as suas necessidades</p>
+				</div><!--texto-->
+				<div class="flex1">
+					<div class="info">
+						<div class="img" style="background-image: url('imagens/servico1.png');"></div>
+						<div class="txt">
+							<h2>PULSEIRAS DE IDENTIFICAÇÃO</h2>
+							<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</p>
+						</div><!--txt-->
+					</div><!--info-->
+
+					<div class="info">
+						<div class="img" style="background-image: url('imagens/servico2.png');"></div>
+						<div class="txt">
+							<h2>INGRESSOS EM PAPEL</h2>
+							<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</p>
+						</div><!--txt-->
+					</div><!--info-->
+
+					<div class="info">
+						<div class="img" style="background-image: url('imagens/servico3.png');"></div>
+						<div class="txt">
+							<h2>INGRESSOS EM PAPEL</h2>
+							<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</p>
+					</div><!--info-->
+				</div><!--flex1-->
+			</div><!--container-->
+		</section><!--four-->
+
+		<div class="clean"></div>
+
+		<div class="Botao"><a href="#">VER MAIS CURSOS</a></div>
+
+		<section class="five">
+			<div class="cover">
+				<div class="container">
+					<div class="servicos">
+						<h2>"Soluções sob medida para suas <br/>necessidades. Adorei!"</h2>
+						<h1>Alberto Santos. Rio de Janeiro</h1>
+					</div><!--servicos-->
+				</div><!--container-->
+			</div><!--cover-->
+		</section><!--tree-->
+
+		<div class="clean"></div>
+
+		<section class="six">
+			<div class="container">
+				<div class="flex1">
+					<div class="texto">
+						<div class="logo2"></div>
+						<div class="txt1">
+							<p>Lorem ipsum dolor sit amet, consectetuer bh adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam laoreet dolore magna aliquam.</p>
+						</div><!--txt-->
+						<div class="contacto">
+							<div class="info">
+								<h2>Atendimento</h2>
+								<p>(21)3455-5678</p>
+							</div><!--info-->
+							<div class="info">
+								<h2>Contacto</h2>
+								<p>contacto@multiticket.com.br</p>
+							</div><!--info-->
+							<div class="info">
+								<h2 style="margin-left: 45px;">Siga-nos</h2>
+								<div style="text-align: center;">
+									<a href="#" style="color: orange;font-size: 20px;"><i class="fab fa-facebook"></i></a>
+									<a href="#" style="color: orange;font-size: 20px;"><i class="fab fa-whatsapp"></i></a>
+								</div>
+							</div><!--info-->
+						</div><!--contacto-->
+						<div class="txt2">
+							<p>Av. Queiroz Filho, 1560, Bloco Sabie, CJ. 410-CEP 05319-000 <br/>São Paulo, SP, Brasil</p>
+						</div><!--txt-->
+					</div><!--texto-->
+					<div class="espaco"></div>
+					<div class="form">
+						<p>Como podemos te ajudar?</p>
+						<form>
+							<input type="text" name="nome" placeholder="Nome">
+							<input type="text" name="email" placeholder="E-mail">
+							<input type="text" name="telefone" placeholder="Telefone">
+							<textarea placeholder="Mensagem"></textarea>
+							<div style="text-align: right;"><input type="submit" name="acao" value="ENVIAR MENSAGEM"></div>
+						</form>
+					</div><!--form-->
+				</div><!--flex1-->
+
+				<div class="footer">
+					<div class="flex1">
+						<div class="left">
+							<p> C 2020 www.multiticket.com.br Designer HTML5 & CSS3</p>
+						</div><!--left-->
+						<div class="right">
+							<p>Desenvolvido por <strong>Domingos JamAdri</strong></p>
+						</div><!--right-->
+					</div><!--flex1-->
+				</div><!--footer-->
+			</div><!--container-->
+		</section><!--six-->
+
+
+		<script src="js/jquery.js"></script>
+		<script src="js/menu_responsivo.js"></script>
 
 </body>
 </html>
