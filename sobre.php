da4820cf750830d72c383fe483c7e25a8bddec54
domingosjadriano <143624898+domingosjadriano@users.noreply.github.com>
2023-08-31 09:07:15 -0300

    Create sobre.php

---
+++ sobre.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Página Inicial da Multitocket</title>
+		<meta charset="utf-8">
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<link rel="stylesheet" type="text/css" href="css/all.min.css">
+		<meta name="description" content="sitecriadopordomingosadriano-domingosadriano123@gmail.com">
+		<meta name="keywords" content="ilidio,pureza,fotografo">
+		<meta name="author" content="Domingos JamAdri">
+		<meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0" />
+</head>
+<body>
+
+	<header>
+			<nav>
+				<div class="container">
+					<div class="flex1">
+						<div class="img-logo">
+							<div class="logo"></div><!--logo-->
+						</div>
+							<div class="menu">
+								<ul>
+									<li class="bottom"><a href="index.php">INICIO</a></li>
+									<li><a href="#">SOBRE</a></li>
+									<li><a href="#">SERVIÇOS</a></li>
+									<li><a href="portfolio.php">PORTFOLIO</a></li>
+									<li><a href="#">CONTACTO</a></li>
+								</ul>
+								<div class="rede">
+									<a href="#" style="color: orange;font-size: 20px;"><i class="fab fa-facebook"></i></a>
+									<a href="#" style="color: orange;font-size: 20px;"><i class="fab fa-whatsapp"></i></a>
+								</div><!--rede-->
+							</div><!--menu-->
+
+							<div class="menu-mobile">
+								<i class="fa fa-bars"></i>
+								<ul>
+									<li><a href="index.php">INICIO</a></li>
+									<li><a href="#">SOBRE</a></li>
+									<li><a href="#">SERVIÇOS</a></li>
+									<li><a href="portfolio.php">PORTFOLIO</a></li>
+									<li><a href="#">CONTACTO</a></li>
+								</ul>
+
+								<div class="rede">
+									<a href="#" style="color: orange;font-size: 20px;"><i class="fab fa-facebook"></i></a>
+									<a href="#" style="color: orange;font-size: 20px;"><i class="fab fa-whatsapp"></i></a>
+								</div><!--rede-->
+							</div><!--menu-mobile-->
+						</div><!--flex1-->
+
+					<div class="banner">
+						<div class="flex1">
+							<div class="texto">
+								<h2>Pulseiras</h2>
+								<h3>Pesonalizadas</h3>
+								<p>Seu evento ganha uma cara profisional e seus <br>clientes tem ótimas lembranças.</p>
+							</div><!--texto-->
+							<div class="img"></div><!--img-->
+						</div><!--flex1-->
+					</div><!--banner-->
+
+				</div><!--container-->
+			</nav>
+		</header>
+
+		<div class="clean"></div>
+
+		<div class="Botao"><a href="#">VER MAIS CURSOS</a></div>
+
+		<section class="five">
+			<div class="cover">
+				<div class="container">
+					<div class="servicos">
+						<h2>"Soluções sob medida para suas <br/>necessidades. Adorei!"</h2>
+						<h1>Alberto Santos. Rio de Janeiro</h1>
+					</div><!--servicos-->
+				</div><!--container-->
+			</div><!--cover-->
+		</section><!--tree-->
+
+		<div class="clean"></div>
+
+		<section class="six">
+			<div class="container">
+				<div class="flex1">
+					<div class="texto">
+						<div class="logo2"></div>
+						<div class="txt1">
+							<p>Lorem ipsum dolor sit amet, consectetuer bh adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam laoreet dolore magna aliquam.</p>
+						</div><!--txt-->
+						<div class="contacto">
+							<div class="info">
+								<h2>Atendimento</h2>
+								<p>(21)3455-5678</p>
+							</div><!--info-->
+							<div class="info">
+								<h2>Contacto</h2>
+								<p>contacto@multiticket.com.br</p>
+							</div><!--info-->
+							<div class="info">
+								<h2 style="margin-left: 45px;">Siga-nos</h2>
+								<div style="text-align: center;">
+									<a href="#" style="color: orange;font-size: 20px;"><i class="fab fa-facebook"></i></a>
+									<a href="#" style="color: orange;font-size: 20px;"><i class="fab fa-whatsapp"></i></a>
+								</div>
+							</div><!--info-->
+						</div><!--contacto-->
+						<div class="txt2">
+							<p>Av. Queiroz Filho, 1560, Bloco Sabie, CJ. 410-CEP 05319-000 <br/>São Paulo, SP, Brasil</p>
+						</div><!--txt-->
+					</div><!--texto-->
+					<div class="espaco"></div>
+					<div class="form">
+						<p>Como podemos te ajudar?</p>
+						<form>
+							<input type="text" name="nome" placeholder="Nome">
+							<input type="text" name="email" placeholder="E-mail">
+							<input type="text" name="telefone" placeholder="Telefone">
+							<textarea placeholder="Mensagem"></textarea>
+							<div style="text-align: right;"><input type="submit" name="acao" value="ENVIAR MENSAGEM"></div>
+						</form>
+					</div><!--form-->
+				</div><!--flex1-->
+
+				<div class="footer">
+					<div class="flex1">
+						<div class="left">
+							<p> C 2020 www.multiticket.com.br Designer HTML5 & CSS3</p>
+						</div><!--left-->
+						<div class="right">
+							<p>Desenvolvido por <strong>Domingos JamAdri</strong></p>
+						</div><!--right-->
+					</div><!--flex1-->
+				</div><!--footer-->
+			</div><!--container-->
+		</section><!--six-->
+
+
+		<script src="js/jquery.js"></script>
+		<script src="js/menu_responsivo.js"></script>
+
+</body>
+</html>
