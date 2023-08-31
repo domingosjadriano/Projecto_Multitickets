d6f8a6b10233b416bba6f2e6ea6449570a67583e
domingosjadriano <domingosadriano123@gmail.com>
2023-08-31 08:55:37 -0300

    Adicionando novas pastas

---
+++ portfolio.php
@@ -0,0 +1,168 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Página Inicial da Multitocket</title>
+		<meta charset="utf-8">
+		<link rel="stylesheet" type="text/css" href="css/portfolio.css">
+		<link rel="stylesheet" type="text/css" href="css/all.min.css">
+		<meta name="description" content="sitecriadopordomingosadriano-domingosadriano123@gmail.com">
+		<meta name="keywords" content="ilidio,pureza,fotografo">
+		<meta name="author" content="Domingos JamAdri">
+		<meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0" />
+	</head>
+	<body>
+		<header>
+			<nav>
+				<div class="container">
+					<div class="flex1">
+						<div class="img-logo">
+							<div class="logo"></div><!--logo-->
+						</div>
+							<div class="menu">
+								<ul>
+									<li><a href="index.php">INICIO</a></li>
+									<li><a href="#">SOBRE</a></li>
+									<li><a href="#">SERVIÇOS</a></li>
+									<li class="bottom"><a href="#">PORTFOLIO</a></li>
+									<li><a href="#">CONTACTO</a></li>
+								</ul>
+								<div class="rede">
+									<a href="#" style="color: orange;font-size: 20px;"><i class="fab fa-facebook"></i></a>
+									<a href="#" style="color: orange;font-size: 20px;"><i class="fab fa-whatsapp"></i></a>
+								</div><!--rede-->
+							</div><!--menu-->
+
+							<div class="menu-mobile">
+								<i class="fa fa-bars"></i>
+								<ul>
+									<li><a href="index.php">INICIO</a></li>
+									<li><a href="#">SOBRE</a></li>
+									<li><a href="#">SERVIÇOS</a></li>
+									<li><a href="#">PORTFOLIO</a></li>
+									<li><a href="#">CONTACTO</a></li>
+								</ul>
+
+								<div class="rede">
+									<a href="#" style="color: orange;font-size: 20px;"><i class="fab fa-facebook"></i></a>
+									<a href="#" style="color: orange;font-size: 20px;"><i class="fab fa-whatsapp"></i></a>
+								</div><!--rede-->
+							</div><!--menu-mobile-->
+						</div><!--flex1-->
+
+					<div class="banner">
+						<div class="texto">
+							<h2>PORTFOLIO</h2>
+						</div><!--texto-->
+						<div class="bottom"></div>
+					</div><!--banner-->
+
+				</div><!--container-->
+			</nav>
+		</header>
+
+		<section class="two">
+			<div class="container">
+				<div class="texto">
+					<h1>NOSSOS TRABALHOS</h1>
+					<h2>Conheça nossos <strong>trabalhos</strong></h2>
+					<p>Lorem ipsum dolor sit amet, consectetuer elit, sed diam nonummy nibh euismod sit tincidunt ut laoreet dolore magna aliquam esse molestie consequatadipiscing erat volelit esse molestie consequatadipiscing Lorem ipsum dolor sit amet, consectetuer elit, sed diam nonummy nibh euismod tincidunt ut laoreet sit dolore magna.</p>
+				</div><!--texto-->
+
+				<div class="carrosel">
+					<div class="banner1">
+						<div class="flex1">
+							<div class="seta"><--</div>
+							<div class="trabalho"></div>
+							<div class="seta">--></div>
+						</div><!--flex1-->
+					</div><!--banner1-->
+
+						<div class="clean"></div>
+
+					<div class="banner2">
+						<div class="flex1">
+							<div class="trabalho-single">
+								<div class="wraper" style="background-image: url('imagens/trabalho1.png');"></div><!--wrap-->
+							</div><!--trabalho-single-->
+
+							<div class="trabalho-single">
+								<div class="wraper" style="background-image: url('imagens/trabalho2.png');"></div><!--wrap-->
+							</div><!--trabalho-single-->
+
+							<div class="trabalho-single">
+								<div class="wraper" style="background-image: url('imagens/trabalho1.png');"></div><!--wrap-->
+							</div><!--trabalho-single-->
+
+							<div class="trabalho-single">
+								<div class="wraper" style="background-image: url('imagens/trabalho2.png');"></div><!--wrap-->
+							</div><!--trabalho-single-->
+							
+						</div><!--flex1-->
+					</div><!--banner2-->
+						<div class="clean"></div>
+				</div><!--carrosel-->
+			</div><!--container-->
+		</section><!--two-->
+
+		<div class="clean"></div>
+
+		<section class="six">
+			<div class="container">
+				<div class="flex1">
+					<div class="texto">
+						<div class="logo2"></div>
+						<div class="txt1">
+							<p>Lorem ipsum dolor sit amet, consectetuer bh adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam laoreet dolore magna aliquam.</p>
+						</div><!--txt-->
+						<div class="contacto">
+							<div class="info">
+								<h2>Atendimento</h2>
+								<p>(21)3455-5678</p>
+							</div><!--info-->
+							<div class="info">
+								<h2>Contacto</h2>
+								<p>contacto@multiticket.com.br</p>
+							</div><!--info-->
+							<div class="info">
+								<h2 style="margin-left: 45px;">Siga-nos</h2>
+								<div style="text-align: center;">
+									<a href="#" style="color: orange;font-size: 20px;"><i class="fab fa-facebook"></i></a>
+									<a href="#" style="color: orange;font-size: 20px;"><i class="fab fa-whatsapp"></i></a>
+								</div>
+							</div><!--info-->
+						</div><!--contacto-->
+						<div class="txt2">
+							<p>Av. Queiroz Filho, 1560, Bloco Sabie, CJ. 410-CEP 05319-000 <br/>São Paulo, SP, Brasil</p>
+						</div><!--txt-->
+					</div><!--texto-->
+					<div class="espaco"></div>
+					<div class="form">
+						<p>Como podemos te ajudar?</p>
+						<form>
+							<input type="text" name="nome" placeholder="Nome">
+							<input type="text" name="email" placeholder="E-mail">
+							<input type="text" name="telefone" placeholder="Telefone">
+							<textarea placeholder="Mensagem"></textarea>
+							<div style="text-align: right;"><input type="submit" name="acao" value="ENVIAR MENSAGEM"></div>
+						</form>
+					</div><!--form-->
+				</div><!--flex1-->
+
+				<div class="footer">
+					<div class="flex1">
+						<div class="left">
+							<p> C 2020 www.multiticket.com.br Designer HTML5 & CSS3</p>
+						</div><!--left-->
+						<div class="right">
+							<p>Desenvolvido por <strong>Domingos JamAdri</strong></p>
+						</div><!--right-->
+					</div><!--flex1-->
+				</div><!--footer-->
+			</div><!--container-->
+		</section><!--six-->
+
+
+		<script src="js/jquery.js"></script>
+		<script src="js/menu_responsivo.js"></script>
+	</body>
+</html>
